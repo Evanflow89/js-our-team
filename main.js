dcce570a954b067bd59ec79c90e24c783ca500a9
Evanflow89 <ivanbaragone@gmail.com>
2022-05-14 15:08:34 +0200

    Added Cards

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
 const cardContainer = document.querySelector(".team-container");
 
-cards = [
+const cards = [
     {
         name: "Wayne Barnett",
-        img: "img\wayne-barnett-founder-ceo.jpg",
+        img: "img/wayne-barnett-founder-ceo.jpg",
         role: "Founder & CEO",
+    },
+    {
+        name: "Angela Caroll",
+        img: "img/angela-caroll-chief-editor.jpg",
+        role: "Chief Editor",
+    },
+    {
+        name: "Walter Gordon",
+        img: "img/walter-gordon-office-manager.jpg",
+        role: "Office Manager",
+    },
+    {
+        name: "Angela Lopez",
+        img: "img/angela-lopez-social-media-manager.jpg",
+        role: "Social Media Manager",
+    },
+    {
+        name: "Scott Estrada",
+        img: "img/scott-estrada-developer.jpg",
+        role: "Developer",
+    },
+    {
+        name: "Barbara Ramos",
+        img: "img/barbara-ramos-graphic-designer.jpg",
+        role: "Graphic Designer",
+    },
+];
 
 
-    }
-]
+for (let i = 0; i < cards.lenght; i++) {
+    console.log(cards[i]);
+}
