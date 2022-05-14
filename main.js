ff40bd6c37314eb487b256db68b31dbe78c37cfb
Evanflow89 <ivanbaragone@gmail.com>
2022-05-14 14:58:06 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const cardContainer = document.querySelector(".team-container");
+
+cards = [
+    {
+        name: "Wayne Barnett",
+        img: "img\wayne-barnett-founder-ceo.jpg",
+        role: "Founder & CEO",
+
+
+    }
+]
